dc5c17ddb125bdd7d55e2608a6c6c74d58c635d9
Tobias Löw <tobi.loew@protonmail.ch>
2025-01-31 13:50:35 +0100

    added example: visualize_franka_robot

---
+++ examples/visualize_franka_robot/visualize_franka_robot.cpp
@@ -0,0 +1,26 @@
+#include <gafro/gafro.hpp>
+#include <gafro_robot_descriptions/FrankaEmikaRobot.hpp>
+#include <sackmesser/Callbacks.hpp>
+#include <sackmesser_ros2/Interface.hpp>
+
+int main(int argc, char **argv)
+{
+    auto interface = sackmesser_ros::Interface::create(argc, argv, "visualize_franka_robot", "gafro_ros2");
+
+    gafro::FrankaEmikaRobot<double> panda;
+
+    interface->loop([&]() {
+        Eigen::MatrixXd q = panda.getRandomConfiguration();
+
+        gafro::Motor<double> ee_motor = panda.getEEMotor(q);
+        gafro::Point<double> ee_point = ee_motor.apply(gafro::Point<double>());
+        gafro::Line<double> ee_line = ee_motor.apply(gafro::Line<double>::Z());
+
+        interface->getCallbacks()->invoke("robot", q, gafro::Motor<double>());
+        interface->getCallbacks()->invoke("ee_motor", ee_motor);
+        interface->getCallbacks()->invoke("ee_point", ee_point);
+        interface->getCallbacks()->invoke("ee_line", ee_line);
+    });
+
+    return 1;
+}
---
+++ examples/visualize_franka_robot/visualize_franka_robot_launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import os.path
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='gafro_ros2',
+            namespace='visualize_franka_robot',
+            executable='visualize_franka_robot',
+            name='visualize_franka_robot'
+        ),
+        Node(
+            package='rviz2',
+            namespace='rviz2',
+            executable='rviz2',
+            name='rviz',
+            arguments=['-d' + os.path.join(get_package_share_directory('gafro_ros2'), 'visualize_franka_robot', 'visualize_franka_robot.rviz')]
+        )
+    ])
